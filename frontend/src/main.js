6375debea801bcd4fbe9712f7cb0973c9c4b18cc
FcRig <69701326+FcRig@users.noreply.github.com>
2025-06-10 18:50:50 -0300

    Excluindo vue toasted e inserindo gitignore

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import toasted from 'vue-toasted';
+
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(vuetify);
-app.use(toasted);
 app.mount('#app');
